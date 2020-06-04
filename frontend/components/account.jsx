f95ceaa98b09c13439637811a0a2dd9578775bbb
Isaac Owens <isaac.owens832@gmail.com>
2020-06-04 12:15:16 -0400

    Complete html for account plan section

--- frontend/components/account.jsx
+++ frontend/components/account.jsx
@@ -15,33 +15,41 @@ class Account extends React.Component {
   }
 
   render() {
-    let accountHeader = "account-header-nav";
+    let accountHeader = "account-header";
+    let accountHeaderNav = "account-header-nav";
     let splashHeaderLogo = "splash-header-logo";
-    let splashHeaderLogoLink = "splash-header-logo-link";
-    let accountNavLinks = "account-nav-links";
+    let accountHeaderLogoLink = "account-header-logo-link";
+
+    let accountHeaderNavLinks = "account-header-nav-links";
     let accountMain = "account-main"
     let accountMainContent = "account-main-content";
+    let profileTable = "profile-table";
     let accountSidebar = "account-sidebar";
     let accountInfo = "account-info";
     let accountWelcome = "account-welcome";
     let accountFooter = "account-footer-nav";
 
 
-    let sosoflyLogo = <FontAwesomeIcon icon={faCompactDisc} size="1x" />;
+    let sosoflyLogo = <FontAwesomeIcon icon={faCompactDisc} size="2x" />;
 
     return (
       <>
         <div>
           <header className={accountHeader}>
-            <h1 className={splashHeaderLogo}>
-              <Link to="/" className={splashHeaderLogoLink}>
-                {sosoflyLogo} Sosofly
-              </Link>
-            </h1>
-            <div className={accountNavLinks}>
-              <p>--- Webplayer Link ---</p>
-              <p>--- User Dropdown ---</p>
-            </div>
+            <nav className={accountHeaderNav}>
+              <h1 className={splashHeaderLogo}>
+                <Link to="/">
+                  <div className={accountHeaderLogoLink}>
+                    <span>{sosoflyLogo}</span>
+                    <span>Sosofly</span>
+                  </div>
+                </Link>
+              </h1>
+              <div className={accountHeaderNavLinks}>
+                <p>--- Webplayer Link ---</p>
+                <p>--- User Dropdown ---</p>
+              </div>
+            </nav>
           </header>
         </div>
         <div className={accountMain}>
@@ -50,20 +58,36 @@ class Account extends React.Component {
           </div>
           <main className={accountMainContent}>
             <aside className={accountSidebar}>
-              <h1>--- Sidebar ---</h1>
-              <h1>--- Sidebar ---</h1>
-              <h1>--- Sidebar ---</h1>
-              <h1>--- Sidebar ---</h1>
-              <h1>--- Sidebar ---</h1>
-              <h1>--- Sidebar ---</h1>
-              <h1>--- Sidebar ---</h1>
-              <h1>--- Sidebar ---</h1>
+              <a href="#">
+                <h1>--- Link ---</h1>
+              </a>
+              <a href="#">
+                <h1>--- Link ---</h1>
+              </a>
+              <a href="#">
+                <h1>--- Link ---</h1>
+              </a>
+              <a href="#">
+                <h1>--- Link ---</h1>
+              </a>
+              <a href="#">
+                <h1>--- Link ---</h1>
+              </a>
+              <a href="#">
+                <h1>--- Link ---</h1>
+              </a>
+              <a href="#">
+                <h1>--- Link ---</h1>
+              </a>
+              <a href="#">
+                <h1>--- Link ---</h1>
+              </a>
             </aside>
             <article className={accountInfo}>
-            <h1> Account Overview </h1>
+              <h1> Account Overview </h1>
               <section>
                 <h3>Profile</h3>
-                <table>
+                <table className={profileTable}>
                   <tbody>
                     <tr>
                       <td>
@@ -91,16 +115,45 @@ class Account extends React.Component {
                     </tr>
                   </tbody>
                 </table>
-              </section>
               <p>--- Edit Profile Link ---</p>
+              </section>
+              <section>
+                <h3>Your plan</h3>
+                <div className={accountPlanCardHeader}>
+                  <div className={accountPlanHeader}>
+                    <span>Sosofly Free</span>
+                  </div>
+                  <div></div>
+                </div>
+                <div className={accountPlanCardBody}>
+                  <div>
+                    <p>Enjoy Sosofly free. No ads, no emails, just the flyest music at no cost.</p>
+                  </div>
+                  <div>
+                    <h3>Payment</h3>
+                    <div>Your next bill is never.</div>
+                    <div>
+                      <div>
+                        Your card info here
+                      </div>
+                      <div>
+                        Expires: never
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
             </article>
           </main>
         </div>
         <footer>
           <nav className={accountFooter}>
             <h2>
-              <Link to="/" className={splashHeaderLogoLink}>
-                {sosoflyLogo} Sosofly
+              <Link to="/">
+                <div className={accountHeaderLogoLink}>
+                  <span>{sosoflyLogo}</span>
+                  <span>Sosofly</span>
+                </div>
               </Link>
             </h2>
             <p>--- Webplayer Link Here ---</p>
