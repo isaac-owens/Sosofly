458cdff917421f1f4061ecdce5109babd60ab92c
Isaac Owens <isaac.owens832@gmail.com>
2020-06-04 09:31:05 -0400

    Reorganize css for splash

--- frontend/components/account.jsx
+++ frontend/components/account.jsx
@@ -15,20 +15,45 @@ class Account extends React.Component {
   }
 
   render() {
+    let splashHeader = "splash-header-nav";
+    let splashHeaderLogo = "splash-header-logo";
+    let splashHeaderLogoLink = "splash-header-logo-link";
+    let splashFooter = "splash-footer-nav";
+    let accountHeaderNav = "account-header-nav";
+    let accountFooterNav = "account-footer-nav";
+
+
+    let sosoflyLogo = <FontAwesomeIcon icon={faCompactDisc} size="1x" />;
+
     return (
-      <div>
-        <header className={splashHeader}>
-          <h1 className={splashHeaderLogo}>
-            <Link to="/" className={splashHeaderLogoLink}>
-              {sosoflyLogo} Sosofly
-            </Link>
-          </h1>
-          <div>
-            <p>--- Webplayer Link ---</p>
-            <p>--- User Dropdown ---</p>
+      <>
+        <div>
+          <header className={splashHeader}>
+            <h1 className={splashHeaderLogo}>
+              <Link to="/" className={splashHeaderLogoLink}>
+                {sosoflyLogo} Sosofly
+              </Link>
+            </h1>
+            <div className={accountHeaderNav}>
+              <p>--- Webplayer Link ---</p>
+              <p>--- User Dropdown ---</p>
+            </div>
+            </header>
           </div>
-          </header>
-        </div>
+          <div>
+            <h1>--- Main Content Here ---</h1>
+          </div>
+          <footer className={accountFooterNav}>
+            <nav className={splashFooter}>
+              <h2>
+                <Link to="/" className={splashHeaderLogoLink}>
+                  {sosoflyLogo} Sosofly
+                </Link>
+              </h2>
+              <p>--- Webplayer Link Here ---</p>
+            </nav>
+          </footer>
+        </>
     ); 
   };
 };
