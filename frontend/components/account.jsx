a4be9b7951eadfd84276b37360ef87691d279ce5
Isaac Owens <isaac.owens832@gmail.com>
2020-06-04 10:56:49 -0400

    Complete User Profile table

--- frontend/components/account.jsx
+++ frontend/components/account.jsx
@@ -21,6 +21,9 @@ class Account extends React.Component {
     let accountNavLinks = "account-nav-links";
     let accountMain = "account-main"
     let accountMainContent = "account-main-content";
+    let accountSidebar = "account-sidebar";
+    let accountInfo = "account-info";
+    let accountWelcome = "account-welcome";
     let accountFooter = "account-footer-nav";
 
 
@@ -42,8 +45,55 @@ class Account extends React.Component {
           </header>
         </div>
         <div className={accountMain}>
-          <main className={accountMainContent}>
+          <div className={accountWelcome}>
             <h1>Welcome {this.props.currentUser.username}</h1>
+          </div>
+          <main className={accountMainContent}>
+            <aside className={accountSidebar}>
+              <h1>--- Sidebar ---</h1>
+              <h1>--- Sidebar ---</h1>
+              <h1>--- Sidebar ---</h1>
+              <h1>--- Sidebar ---</h1>
+              <h1>--- Sidebar ---</h1>
+              <h1>--- Sidebar ---</h1>
+              <h1>--- Sidebar ---</h1>
+              <h1>--- Sidebar ---</h1>
+            </aside>
+            <article className={accountInfo}>
+            <h1> Account Overview </h1>
+              <section>
+                <h3>Profile</h3>
+                <table>
+                  <tbody>
+                    <tr>
+                      <td>
+                        <span>Email</span>
+                      </td>
+                      <td>
+                        <p>isaac.owens832@gmail.com</p>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <span>Date of birth</span>
+                      </td>
+                      <td>
+                        <p>10/05/1988</p>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <span>Country</span>
+                      </td>
+                      <td>
+                        <p>US</p>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </section>
+              <p>--- Edit Profile Link ---</p>
+            </article>
           </main>
         </div>
         <footer>
