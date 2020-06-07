974cab77c35c114ad0cc5ae471cb8c2d9dca8f6f
Isaac Owens <isaac.owens832@gmail.com>
2020-06-07 05:10:13 -0400

    Add structure and classnames for main section

--- frontend/components/webplayer.jsx
+++ frontend/components/webplayer.jsx
@@ -23,7 +23,20 @@ class Webplayer extends React.Component {
     let webplayerPlayBar = "webplayer-play-bar"
     let webplayerPlayBarFooter = "webplayer-play-bar-footer";
     let webplayerPlayBarContentContainer = "webplayer-play-bar-content-container";
-    
+    let webplayerMainContainer = "webplayer-main-container";
+    let webplayerScrollNode = "webplayer-scroll-node";
+    let webplayerScrollNodeChildSpacer = "webplayer-scroll-node-child-spacer";
+    let webplayerScrollNodeChild = "webplayer-scroll-node-child";
+
+    let webplayerClassNames = ["webplayer-main-grid", "content-spacing"];
+    webplayerClassNames = webplayerClassNames.join(' ');
+
+    let PlaylistCollection = "playlist-collection";
+    let PlaylistCollectionGrid = "playlist-collection-grid";
+    let PlaylistCollectionHeader = "playlist-collection-header";
+    let GenreHeaderLink = "genre-header-link";
+    let GenreIndexLink = "genre-index-link";
+
     return (
       <div className={webplayerTopContainer}>
         <div className={webplayerTopBar}>
@@ -32,30 +45,53 @@ class Webplayer extends React.Component {
               <div className={webplayerTopBarInnerTransition}></div>
             </div>
             <div className={webplayerTopBarHeaderNavLinks}>
-              <button>
-                back icon here
-              </button>
-              <button>
-                forward icon here
-              </button>
+              <button>back icon here</button>
+              <button>forward icon here</button>
             </div>
           </header>
         </div>
         <div className={webplayerNavBar}>
           <nav className={webplayerNavBarContentContainer}>
             <div className={webplayerNavBarLogolinkContainer}>
-              <Link className={webplayerNavBarLogolink}>
-              </Link>
+              <Link className={webplayerNavBarLogolink}></Link>
             </div>
           </nav>
         </div>
         <div className={webplayerPlayBar}>
           <footer className={webplayerPlayBarFooter}>
-            <div className={webplayerPlayBarContentContainer}></div>
+            <div className={webplayerPlayBarContentContainer}>
+              <div className={webplayerPlaybarLeft}></div>
+              <div className={webplayerPlaybarCenter}></div>
+              <div className={webplayerPlaybarRight}></div>
+            </div>
           </footer>
         </div>
+        <div className={webplayerMain}>
+          <div className={webplayerMainContainer}>
+            <div className={webplayerScrollNode}>
+              <div className={webplayerScrollNodeChildSpacer}>
+                <div className={webplayerScrollNodeChild}>
+                  <div className={webplayerClassNames}>
+                    {/* repeat below <section> for rows of collections */}
+                    <section className={PlaylistCollection}>
+                      {/* extra divs with formatting */}
+                      <div className={PlaylistCollectionGrid}>
+                        <h2 className={PlaylistCollectionHeader}>
+                          <Link className={GenreHeaderLink}>genre title</Link>
+                        </h2>
+                      </div>
+                      <Link className={GenreIndexLink}>
+                        <span>See all</span>
+                      </Link>
+                    </section>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   };
 };
 
