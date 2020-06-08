13e48be5034d0624bedbdcb0b3bbdfe907d9455b
Isaac Owens <isaac.owens832@gmail.com>
2020-06-08 11:06:01 -0400

    Complete splash page

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -21,10 +21,12 @@ class Splash extends React.Component {
     let splashHeader = "splash-header";
     let splashHeaderNav = "splash-header-nav";
     let headerLogoLinkWrapper = "header-logo-link-wrapper";
-    let authLinks = "auth-links";
     let headerLogoLink = "header-logo-link";
-    let headerLink = "header-link"
-
+    let headerLogoLinkTitle = "header-logo-link-title"
+    let authLinks = "auth-links";
+    let authLinksList = "auth-links-list";
+    let authLinksListItem = "auth-links-list-item";
+    let separator = "separator";
     
     let splashMainHero = "splash-main-hero";  
     let splashMainContent = "splash-main-content";
@@ -40,64 +42,79 @@ class Splash extends React.Component {
     let footerLink = "footer-link";
     let footerWebplayerLink = "footer-webplayer-link";
 
-    let sosoflyHeaderLogo = <FontAwesomeIcon icon={faCompactDisc} size="1x" />;
+    let sosoflyHeaderLogo = <FontAwesomeIcon icon={faCompactDisc} size="3x" />;
     let sosoflyFooterLogo = <FontAwesomeIcon icon={faCompactDisc} size="3x" />;
 
     return (
-    <>
-      <div className={splashMainWrapper}>
-        <header className={splashHeader}>
-          <div className={splashHeaderNav}>
-            <div className={headerLogoLinkWrapper}>
-              <Link to="/" className={headerLogoLink}>
-                {sosoflyHeaderLogo} Sosofly
-              </Link>
+      <>
+        <div className={splashMainWrapper}>
+          <header className={splashHeader}>
+            <div className={splashHeaderNav}>
+              <div className={headerLogoLinkWrapper}>
+                <Link to="/" className={headerLogoLink}>
+                  {sosoflyHeaderLogo} 
+                  <span className={headerLogoLinkTitle}>Sosofly</span>
+                </Link>
+              </div>
+              <nav className={authLinks}>
+                <ul className={authLinksList}>
+                  <li>
+                    <Link to="/webplayer" className={authLinksListItem}>
+                      Webplayer
+                    </Link>
+                  </li>
+                  <li className={authLinksListItem}>Help</li>
+                  <li className={authLinksListItem}>Download</li>
+                  <li role="separator" className={separator}></li>
+                  <li>
+                    <Link to="/signup" className={authLinksListItem}>
+                      Signup
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/login" className={authLinksListItem}>
+                      Log In
+                    </Link>
+                  </li>
+                </ul>
+              </nav>
             </div>
-            <div className={authLinks}>
-              <Link to="/signup" className={headerLink}>
-                Signup
-              </Link>
-              <Link to="/login" className={headerLink}>
-                Log In
-              </Link>
+          </header>
+          <main className={splashMainHero}>
+            <picture>
+              <img
+                className={splashImage}
+                src={window.heroURL}
+                alt="woman wearing headphones looking to the sky"
+              />
+            </picture>
+            <div className={splashMainContent}>
+              <section className={splashMainSection}>
+                <h1 className={splashMainSectionHeader}>
+                  Listening is everything
+                </h1>
+                <h4 className={splashMainSectionSubheader}>
+                  Millions of songs. No credit card needed.
+                </h4>
+                <Link to="/signup" className={splashMainSignupButton}>
+                  GET SOSOFLY FREE
+                </Link>
+              </section>
             </div>
-          </div>
-        </header>
-        <main className={splashMainHero}>
-          <picture>
-            <img
-              className={splashImage}
-              src={window.heroURL}
-              alt="woman wearing headphones looking to the sky"
-            />
-          </picture>
-          <div className={splashMainContent}>
-            <section className={splashMainSection}>
-              <h1 className={splashMainSectionHeader}>
-                Listening is everything
-              </h1>
-              <h4 className={splashMainSectionSubheader}>
-                Millions of songs. No credit card needed.
-              </h4>
-              <Link to="/signup" className={splashMainSignupButton}>
-                GET SOSOFLY FREE
+          </main>
+        </div>
+        <footer className={splashFooter}>
+          <nav className={splashFooterNav}>
+            <h2>
+              <Link to="/" className={footerLink}>
+                {sosoflyFooterLogo} Sosofly
               </Link>
-            </section>
-          </div>
-        </main>
-      </div>
-      <footer className={splashFooter}>
-        <nav className={splashFooterNav}>
-          <h2>
-            <Link to="/" className={footerLink}>
-              {sosoflyFooterLogo} Sosofly
+            </h2>
+            <Link to="#" className={footerWebplayerLink}>
+              Webplayer
             </Link>
-          </h2>
-          <Link to="#" className={footerWebplayerLink}>
-            Webplayer
-          </Link>
-        </nav>
-      </footer>
+          </nav>
+        </footer>
       </>
     );
   }
