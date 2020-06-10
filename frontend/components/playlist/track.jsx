c17fe0131829ebf814ae0bb80a1cfa26f3033e92
Isaac Owens <isaac.owens832@gmail.com>
2020-06-09 21:38:21 -0400

    Add artist name and album name as links to track

--- frontend/components/playlist/track.jsx
+++ frontend/components/playlist/track.jsx
@@ -14,24 +14,40 @@ class Track extends React.Component {
     let tracklistPPTopAlign = "tracklist-play-pause-top-align";
     let tracklistTopAlign = "tracklist-top-align";
     let musicDiscContainer = "music-disc-container"
+    let tracklistNameBox = "tracklist-name-box";
+    let tracklistNameWrapper = "tracklist-name-wrapper";
+    let tracklistName = "tracklist-name";
+    let tracklistNameSub = "tracklist-name-sub";
+    let tracklistArtist = "tracklist-artist";
+    let dotSeparator = "dot-separator";
+    let tracklistAlbum = "tracklist-album";
 
     let miniDisc = <FontAwesomeIcon icon={faCompactDisc} size="1x" />;
     return (
       <div className={trackWrapper}>
         <div></div>
-          <li className={tracklistRow}>
-            <div className={tracklistColumnOuter}>
-              <div className={tracklistPPTopAlign}></div>
-              <div className={tracklistTopAlign}>
-                <span className={musicDiscContainer}>{miniDisc}</span>
+        <li className={tracklistRow}>
+          <div className={tracklistColumnOuter}>
+            <div className={tracklistPPTopAlign}></div>
+            <div className={tracklistTopAlign}>
+              <span className={musicDiscContainer}>{miniDisc}</span>
+            </div>
+          </div>
+          <div className={tracklistNameBox}>
+            <div className={tracklistNameWrapper}>
+              <div className={tracklistName}>Track Name</div>
+              <div className={tracklistNameSub}>
+                <span className={tracklistArtist}>I do not own the rights to these tasty jams.</span>
+                <span className={dotSeparator}>•</span>
+                <span className={tracklistAlbum}>Album Name Here</span>
               </div>
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
-          </li>
+          </div>
+          <div></div>
+          <div></div>
+        </li>
       </div>
-    )
+    );
   }
 }
 
