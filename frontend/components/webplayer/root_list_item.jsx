42e170d3ba5d03e826b515dd8f5db1d22d2c1229
Isaac Owens <isaac.owens832@gmail.com>
2020-06-11 11:10:19 -0400

    Add dropdown structure for side playlist links but no actions for onClicks yet

--- frontend/components/webplayer/root_list_item.jsx
+++ frontend/components/webplayer/root_list_item.jsx
@@ -2,34 +2,85 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 class RootListItem extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+    this.container = React.createRef();
+    this.state = {
+      open: false,
+    };
+    this.handleLinkClick = this.handleLinkClick.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleDelete = this.handleUpdate.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  handleLinkClick() {
+    this.setState({ open: !this.state.open });
+    return false;
+  }
+
+  handleDelete() {}
+
+  handleUpdate() {}
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.handleClickOutside());
+  }
+  
+  handleClickOutside() {
+    return (e) => {
+      if (
+        this.container.current &&
+        !this.container.current.contains(e.target)
+      ) {
+        this.setState({ open: false });
+      }
+    };
   }
 
   render() {
+    let reactWrapper = "react-wrapper";
+    let RootlistItem = "root=list=item";
+    let textWrapper = "text-wrapper";
+    let playlistTitle = "playlist-title";
+    let playlistDropdown = "playlist-dropdown";
+    let playlistOption = "playlist-option";
 
-  let reactWrapper = "react-wrapper";
-  let RootlistItem = "root=list=item";
-  let textWrapper = "text-wrapper";
-  let playlistTitle = "playlist-title";
-
-  let {playlist, deletePlaylistForm} = this.props;
-
+    let { playlist, deletePlaylistForm } = this.props;
 
     return (
-      <div className={reactWrapper} key={playlist.id} >
-        <li className={RootlistItem}>
+      <div className={reactWrapper} key={playlist.id}>
+        <li
+          onContextMenu={this.handleLinkClick}
+          className={RootlistItem}
+          ref={this.container}
+        >
+          {this.state.open ? (
+            <div className={playlistDropdown}>
+              <ul>
+                <li className={playlistOption} onClick={this.handleDelete}>
+                  Delete
+                </li>
+                <li className={playlistOption} onClick={this.handleUpdate}>
+                  Rename
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <div></div>
+          )}
           <div className={textWrapper}>
-            <Link to={`webplayer/playlist/${playlist.id}`}
-            className={playlistTitle}
+            <Link
+              to={`webplayer/playlist/${playlist.id}`}
+              className={playlistTitle}
             >
-            {playlist.title}
-          </Link>
-          {deletePlaylistForm}
+              {playlist.title}
+            </Link>
+            {deletePlaylistForm}
           </div>
         </li>
       </div>
-    )
+    );
   }
 }
 
