39ab0dd22822762dec4dc3d82177bc956f26439c
Isaac Owens <isaac.owens832@gmail.com>
2020-07-23 18:00:59 -0400

    Add params and show action for artist

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,5 +1,15 @@
 class Api::ArtistsController < ApplicationController
   def show
-    
+    @artist = Artist.find_by(id: params[:artist_id])
+
+    if @artist
+      render :show
+    else
+      render :json @artist.error.full_messages, status 422
+    end
+  end
+
+  def artist_params
+    params.require(:artist).permit(:name, :biography)
   end
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,15 @@
+# == Schema Information
+#
+# Table name: artists
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  biography  :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Artist < ApplicationRecord
-  validates :name
+  validates :name, presence: true
 
-  
+
 end
