743f5c5342e11865383e8925f6b96ed347ba66f1
Isaac Owens <isaac.owens832@gmail.com>
2020-07-27 14:52:13 -0400

    Create artist main component

---
+++ frontend/components/artist/artist_show_main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ArtistShowMain extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Artist Show Main</div>
+    )
+  }
+}
+
+export default ArtistShowMain;
