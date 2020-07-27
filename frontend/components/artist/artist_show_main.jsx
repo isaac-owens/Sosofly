687299bb9c7151a56bd9e1967b0c818f39b57486
Isaac Owens <isaac.owens832@gmail.com>
2020-07-27 15:07:07 -0400

    Add tracklist and styling for Popular section

--- frontend/components/artist/artist_show_main.jsx
+++ frontend/components/artist/artist_show_main.jsx
@@ -12,12 +12,18 @@ class ArtistShowMain extends React.Component {
           <div className="artist-main-section">
             <section className="artist-toptracks">
               <h1 className="artist-section-header">Popular</h1>
+              <section className="artist-tracklist-container">
+                <ol className="artist-tracklist">
+                  <li>Track One</li>
+                  <li>Track Two</li>
+                  <li>Track Three</li>
+                </ol>
+              </section>
             </section>
           </div>
         </section>
         <section className="artist-albums">
           <div classNam="artist-main-section">
-
           </div>
         </section>
         <section className="artist-singles artist-albums">
