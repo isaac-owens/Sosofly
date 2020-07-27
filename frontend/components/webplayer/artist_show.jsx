5b1ef5b320c06f387a6e29cd549c9751f562d659
Isaac Owens <isaac.owens832@gmail.com>
2020-07-26 22:35:25 -0400

    Add section and banner divs to artist page

--- frontend/components/webplayer/artist_show.jsx
+++ frontend/components/webplayer/artist_show.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
-class ArtistShow extends React.Component {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlayCircle } from "@fortawesome/free-regular-svg-icons";
 
+
+class ArtistShow extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <section className="artist-showpage-top-container">
+        <div className="artist-showpage-banner">
+          <div className="artist-showpage-banner-img">
+
+          </div>
+        </div>
+      </section>
+    )
+  }
 }
 
 export default ArtistShow;
