06e8ed7502fc69c331e7aed0fd6efabefa55ed41
Isaac Owens <isaac.owens832@gmail.com>
2020-07-27 15:02:39 -0400

    Add styling for artist page sections and headers

--- frontend/components/artist/artist_show_main.jsx
+++ frontend/components/artist/artist_show_main.jsx
@@ -8,10 +8,28 @@ class ArtistShowMain extends React.Component {
   render() {
     return (
       <div>
-        <section className="artist-music"></section>
-        <section className="artist-albums"></section>
-        <section className="artist-singles artist-albums"></section>
-        <section className="artist-appears-on artist-albums"></section>
+        <section className="artist-music">
+          <div className="artist-main-section">
+            <section className="artist-toptracks">
+              <h1 className="artist-section-header">Popular</h1>
+            </section>
+          </div>
+        </section>
+        <section className="artist-albums">
+          <div classNam="artist-main-section">
+
+          </div>
+        </section>
+        <section className="artist-singles artist-albums">
+          <div classNam="artist-main-section">
+
+          </div>
+        </section>
+        <section className="artist-appears-on artist-albums">
+          <div classNam="artist-main-section">
+
+          </div>
+        </section>
       </div>
     )
   }
