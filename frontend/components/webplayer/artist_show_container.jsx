5746860261340c0253b1e11652300360f5abffe2
Isaac Owens <isaac.owens832@gmail.com>
2020-07-28 12:30:22 -0400

    Remove debugger

--- frontend/components/webplayer/artist_show_container.jsx
+++ frontend/components/webplayer/artist_show_container.jsx
@@ -3,7 +3,6 @@ import ArtistShow from '../../components/webplayer/artist_show';
 import { fetchArtist } from '../../actions/artist_actions';
 
 const mSTP = state => {
-  // debugger
   return {
     artist: Object.values(state.entities.artist)[0],
   }
