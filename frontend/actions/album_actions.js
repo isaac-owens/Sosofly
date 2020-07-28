40ceab0fe1d09e7c916c06b9f7b2ec61f0afa163
Isaac Owens <isaac.owens832@gmail.com>
2020-07-28 14:01:54 -0400

    Create actions for album

---
+++ frontend/actions/album_actions.js
@@ -0,0 +1,19 @@
+export const RECEIVE_ALBUM = "RECEIVE_ALBUM";
+
+import * APIUtils from '../util/albums_api_utils';
+
+// regular actions
+
+export const receiveAlbum = (album) => {
+  return {
+    type: RECEIVE_ARTIST,
+    album
+  };
+};
+
+// thunk actions
+
+export const fetchArtist = (albumId) => dispatch => {
+  return APIUtils.fetchArtist(albumId)
+    .then(album => dispatch(receiveArtist(album)));
+}
