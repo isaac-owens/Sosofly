bd78006814c6cdedec00dea585b8b5a304603b06
Isaac Owens <isaac.owens832@gmail.com>
2020-07-28 19:55:06 -0400

    Create mapping for album show container

---
+++ frontend/components/album/album_show_container.jsx
@@ -0,0 +1,16 @@
+import { connect } from 'react-redux';
+import AlbumShow from '../../components/album/album_show';
+
+const mSTP = state => {
+  return {
+
+  }
+}
+
+const mDTP = dispatch => {
+  return {
+
+  }
+}
+
+export default connect(mSTP, mDTP)(AlbumShow)
