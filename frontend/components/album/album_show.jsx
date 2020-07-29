2301dfb9edd54ce6f55b699c5db741935c968d14
Isaac Owens <isaac.owens832@gmail.com>
2020-07-28 20:01:12 -0400

    Add album and fetchAlbum to album container

---
+++ frontend/components/album/album_show.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+
+class AlbumShow extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>Album Show Page!</div>
+    )
+
+    // if (
+    //   !album
+    // ) {
+    //   return <div></div>;
+    // } else {
+    //   return (
+    //     <section className={playlistShowPageTopContainer}>
+    //       <div className={playlistShowHeader}>
+    //         <div className={playlistShowHeaderSetup}></div>
+    //         <div className={playlistShowHeaderGradient}></div>
+    //         <div></div>
+    //         <div className={playlistShowHeaderImageBox}>
+    //           <div className={playlistShowHeaderImage}>
+    //             <img
+    //               src={playlist.image}
+    //               alt="playlist image"
+    //               className={playlistShowHeaderImage}
+    //             />
+    //           </div>
+    //         </div>
+    //         <div className={playlistShowHeaderBanner}>
+    //           <h2 className={playlistShowHeaderSubBanner}>
+    //             {playlist.title}
+    //           </h2>
+    //           <span className={playlistShowHeaderTitleBox}>
+    //             <h1 className={playlistShowHeaderTitle}>
+    //               {playlist.title}
+    //             </h1>
+    //           </span>
+    //         </div>
+    //       </div>
+    //       <div className={playlistTopColor}></div>
+    //       <div className={playlistControlsBox}>
+    //         <div className={playlistControls}>
+    //           <button className={playlistShowPlayButon}>{playCircle}</button>
+    //           <button className={fireButtonInactive}>{fire}</button>
+    //           <div className={contextMenu}>
+    //             <button className={contextEllipsis}>{ellipsis}</button>
+    //           </div>
+    //         </div>
+    //       </div>
+    //       <PlaylistShowMain
+    //         id={this.id}
+    //         fetchPlaylistTracks={fetchPlaylistTracks}
+    //         tracks={tracks}
+    //         saveNowPlaying={saveNowPlaying}
+    //       />
+    //     </section>
+    //   );
+    // }
+  }
+}
+
+export default AlbumShow;
--- frontend/components/album/album_show_container.jsx
+++ frontend/components/album/album_show_container.jsx
@@ -1,15 +1,16 @@
 import { connect } from 'react-redux';
 import AlbumShow from '../../components/album/album_show';
+import { fetchAlbum } from '../../actions/album_actions';
 
 const mSTP = state => {
   return {
-
+    album: Object.values(state.entities.album)[0],
   }
 }
 
 const mDTP = dispatch => {
   return {
-
+    fetchAlbum: albumId => dipsatch(fetchAlbum(albumId)),
   }
 }
 
