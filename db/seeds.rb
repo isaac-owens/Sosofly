c4ed3b6aaca3d5825d66fa00e89202382ca584ab
Isaac Owens <isaac.owens832@gmail.com>
2020-08-03 13:38:06 -0400

    Add 'duration' to all hip-hop tracks

--- db/seeds.rb
+++ db/seeds.rb
@@ -86,42 +86,42 @@ electronic.image.attach(io: file, filename: 'electronic.jpeg')
 
 # HIP HOP TRACKS
 
-all_that = Track.create(title: "All That", artist_id: kendrick_lamar.id, album_id: damn.id)
+all_that = Track.create(title: "All That", artist_id: kendrick_lamar.id, album_id: damn.id, duration: "2:25")
 file = open('https://sosofly-seeds.s3.us-east-2.amazonaws.com/audio/bensound-allthat.mp3')
 all_that.track_file.attach(io: file, filename: 'bensound-allthat.mp3')
 AddedTrack.create!(playlist_id: hip_hop.id, track_id: all_that.id)
 
-bad_ass = Track.create(title: "Bad Ass", artist_id: flying_lotus.id, album_id: cosmogramma.id)
+bad_ass = Track.create(title: "Bad Ass", artist_id: flying_lotus.id, album_id: cosmogramma.id, duration: "1:49")
 file = open('https://sosofly-seeds.s3.us-east-2.amazonaws.com/audio/bensound-badass.mp3')
 bad_ass.track_file.attach(io: file, filename: 'bensound-badass.mp3')
 AddedTrack.create!(playlist_id: hip_hop.id, track_id: bad_ass.id)
 
-downtown = Track.create(title: "Downtown", artist_id: kendrick_lamar.id, album_id: damn.id)
+downtown = Track.create(title: "Downtown", artist_id: kendrick_lamar.id, album_id: damn.id, duration: "")
 file = open('https://sosofly-seeds.s3.us-east-2.amazonaws.com/audio/bensound-downtown.mp3')
 downtown.track_file.attach(io: file, filename: 'bensound-downtown.mp3')
 AddedTrack.create!(playlist_id: hip_hop.id, track_id: downtown.id)
 
-dreams = Track.create(title: "Dreams", artist_id: flying_lotus.id, album_id: cosmogramma.id)
+dreams = Track.create(title: "Dreams", artist_id: flying_lotus.id, album_id: cosmogramma.id, duration: "")
 file = open('https://sosofly-seeds.s3.us-east-2.amazonaws.com/audio/bensound-dreams.mp3')
 dreams.track_file.attach(io: file, filename: 'bensound-dreams.mp3')
 AddedTrack.create!(playlist_id: hip_hop.id, track_id: dreams.id)
 
-endless_motion = Track.create(title: "Endless Motion", artist_id: flying_lotus.id, album_id: cosmogramma.id)
+endless_motion = Track.create(title: "Endless Motion", artist_id: flying_lotus.id, album_id: cosmogramma.id, duration: "")
 file = open('https://sosofly-seeds.s3.us-east-2.amazonaws.com/audio/bensound-endlessmotion.mp3')
 endless_motion.track_file.attach(io: file, filename: 'bensound-endlessmotion.mp3')
 AddedTrack.create!(playlist_id: hip_hop.id, track_id: endless_motion.id)
 
-erf = Track.create(title: "E.R.F.", artist_id: kendrick_lamar.id, album_id: damn.id)
+erf = Track.create(title: "E.R.F.", artist_id: kendrick_lamar.id, album_id: damn.id, duration: "")
 file = open('https://sosofly-seeds.s3.us-east-2.amazonaws.com/audio/bensound-erf.mp3')
 erf.track_file.attach(io: file, filename: 'bensound-erf.mp3')
 AddedTrack.create!(playlist_id: hip_hop.id, track_id: erf.id)
 
-funky_suspense = Track.create(title: "Funky Suspense", artist_id: flying_lotus.id, album_id: cosmogramma.id)
+funky_suspense = Track.create(title: "Funky Suspense", artist_id: flying_lotus.id, album_id: cosmogramma.id, duration: "")
 file = open('https://sosofly-seeds.s3.us-east-2.amazonaws.com/audio/bensound-funkysuspense.mp3')
 funky_suspense.track_file.attach(io: file, filename: 'bensound-funkysuspense.mp3')
 AddedTrack.create!(playlist_id: hip_hop.id, track_id: funky_suspense.id)
 
-groovy_hiphop = Track.create(title: "Groovy Hip-Hop", artist_id: kendrick_lamar.id, album_id: damn.id)
+groovy_hiphop = Track.create(title: "Groovy Hip-Hop", artist_id: kendrick_lamar.id, album_id: damn.id, duration: "")
 file = open('https://sosofly-seeds.s3.us-east-2.amazonaws.com/audio/bensound-groovyhiphop.mp3')
 groovy_hiphop.track_file.attach(io: file, filename: 'bensound-groovyhiphop.mp3')
 AddedTrack.create!(playlist_id: hip_hop.id, track_id: groovy_hiphop.id)
