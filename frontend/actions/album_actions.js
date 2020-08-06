64381485cf0b1f4b7b406226ccd11a0481f9f9b5
Isaac Owens <isaac.owens832@gmail.com>
2020-08-05 20:13:18 -0400

    Create action for fetching all albums

--- frontend/actions/album_actions.js
+++ frontend/actions/album_actions.js
@@ -1,8 +1,15 @@
 export const RECEIVE_ALBUM = "RECEIVE_ALBUM";
+export const RECEIVE_ALBUMS = "RECEIVE_ALBUMS";
 
 import * as APIUtils from '../util/albums_api_utils';
 
 // regular actions
+export const receiveAlbums = (albums) => {
+  return {
+    type: RECEIVE_ALBUMS,
+    albums
+  }
+}
 
 export const receiveAlbum = (album) => {
   return {
