acb259d204246efc638522b28fd0520cc5778b88
Isaac Owens <isaac.owens832@gmail.com>
2020-08-08 15:54:33 -0400

    Create frame for playlist add component

---
+++ frontend/components/playlist/playlist_add.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PlaylistAdd extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Add playlist modal!</div>
+    )
+  }
+}
+
+export default PlaylistAdd;
