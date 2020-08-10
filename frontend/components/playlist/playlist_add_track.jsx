1214f1e4e2e39672454b5905ce5075f0722e1eb2
Isaac Owens <isaac.owens832@gmail.com>
2020-08-10 16:39:05 -0400

    Fix html structure for add track modal

--- frontend/components/playlist/playlist_add_track.jsx
+++ frontend/components/playlist/playlist_add_track.jsx
@@ -16,18 +16,15 @@ class PlaylistAddTrack extends React.Component {
     let ex = <FontAwesomeIcon icon={faTimes} size="3x" />;
 
     return (
-      <div>
-        
-        <div className="add-track-modal-header-container">
-          <button className="top-cancel-button" onClick={closeModal}>
-            <div className="top-cancel-icon">{ex}</div>
-          </button>
-          <div className="add-track-modal-header">
-            <h1 className="add-track-modal-header-message">Add to playlist</h1>
-          </div>
+      <>
+        <button className="top-cancel-button" onClick={closeModal}>
+          <div className="top-cancel-icon">{ex}</div>
+        </button>
+        <div className="add-track-modal-header">
+          <h1 className="add-track-modal-header-message">Add to playlist</h1>
         </div>
         <div></div>
-      </div>
+      </>
     )
   }
 }
